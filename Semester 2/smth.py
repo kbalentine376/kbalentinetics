18f5af63b1fcecffb39e5bbc905255f3c6f3b4e1
Kaden <kbalentine376@phm.k12.in.us>
2026-01-30 09:47:31 -0500

    Create smth.py

---
+++ Semester 2/smth.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+def say_hi():
+    label.config(text = "You clicked the button")
+
+def say_bye():
+    label.config(text = "Hello TKinter")
+
+#create the main window
+root = tk.Tk()
+root.title("My First TKinter Project!")
+root.geometry("400x300")
+
+label = tk.Label(root, text = "Hello TKinter")
+label.pack()
+
+button = tk.Button(root, text = "Click Me!", command = say_hi)
+button.pack()
+
+button = tk.Button(root, text = "Change Back!", command = say_bye)
+button.pack()
+
+#runs the program above
+root.mainloop()
