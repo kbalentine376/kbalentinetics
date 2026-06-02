c34353f7c375b32dd1b43206c668c104de61024d
Kaden <150942217+kbalentine376@users.noreply.github.com>
2026-06-02 15:53:32 -0400

    Update game_project_5.py
    Made the sizes static numbers

--- Semester 2/game_project_5.py
+++ Semester 2/game_project_5.py
@@ -5,23 +5,31 @@ import random
 # RETRO PONG
 # ==========
 
+WIDTH = 800
+HEIGHT = 500
+
+PADDLE_WIDTH = 15
+PADDLE_HEIGHT = 100
+
+BALL_SIZE = 15
+
 def player(canvas):
-    canvas.create_rectangle(30, 500 // 2 - 50, 30 + 10, 500 // 2 + 50, fill="white")
+    canvas.create_rectangle(30, HEIGHT // 2 - PADDLE_HEIGHT // 2, 30 + PADDLE_WIDTH, HEIGHT // 2 + PADDLE_HEIGHT // 2, fill = "white")
 
 def enemy(canvas):
-    canvas.create_rectangle(800 - 40, 500 // 2 - 50, 800 - 30, 500 // 2 + 50, fill="white")
+    canvas.create_rectangle(WIDTH - 30 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, WIDTH - 30, HEIGHT // 2 + PADDLE_HEIGHT // 2, fill = "white")
 
 def ball(canvas):
-    canvas.create_oval(800 // 2 - 15, 500 // 2 - 15, 800 // 2 + 15, 500 // 2 + 15, fill="white")
+    canvas.create_oval(WIDTH // 2 - BALL_SIZE // 2, HEIGHT // 2 - BALL_SIZE // 2, WIDTH // 2 + BALL_SIZE // 2, HEIGHT // 2 + BALL_SIZE // 2, fill = "white")
 
 def line(canvas):
-    canvas.create_line(400, 0, 400, 500, fill="white", dash=(10, 10))
+    canvas.create_line(WIDTH // 2, 0, WIDTH // 2, HEIGHT, fill = "white", dash = (10, 10))
 
 def game():
     root = tk.Tk()
     root.title("Retro Pong")
 
-    canvas = tk.Canvas(root, width=800, height=500, bg="black")
+    canvas = tk.Canvas(root, width = WIDTH, height = HEIGHT, bg = "black")
     canvas.pack()
 
     player(canvas)
