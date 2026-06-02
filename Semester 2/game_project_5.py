585b5d7b68e5ed8d33f57343d5de410c792476e2
Kaden <150942217+kbalentine376@users.noreply.github.com>
2026-06-02 16:36:15 -0400

    Update game_project_5.py
    finished movement of all parts and collision

--- Semester 2/game_project_5.py
+++ Semester 2/game_project_5.py
@@ -1,11 +1,18 @@
+# I decided to make a retro pong instead of what other game I had in idea as it was quicker and easier. 
+# The shapes, and general movement (the paddels ; Up and Down) was coded by me, as well as the out of bounds / collision
+# I used AI for the enemy paddle, I did not understand how to make it follow the ball and I used it for the key bindings as well
+
+#The controls are W and S, you're on the left.
+
+
 import tkinter as tk
-import random
 
 WIDTH = 800
 HEIGHT = 500
 
 PADDLE_WIDTH = 15
 PADDLE_HEIGHT = 100
+
 BALL_SIZE = 15
 
 
@@ -17,42 +24,110 @@ class Game:
         self.canvas = tk.Canvas(self.root, width = WIDTH, height = HEIGHT, bg = "black")
         self.canvas.pack()
 
-        # center line
         self.canvas.create_line(WIDTH // 2, 0, WIDTH // 2, HEIGHT, fill = "white", dash = (10, 10))
 
-        # paddles
         self.player = self.canvas.create_rectangle(30, HEIGHT // 2 - PADDLE_HEIGHT // 2, 30 + PADDLE_WIDTH, HEIGHT // 2 + PADDLE_HEIGHT // 2, fill = "white")
 
         self.enemy = self.canvas.create_rectangle(WIDTH - 30 - PADDLE_WIDTH, HEIGHT // 2 - PADDLE_HEIGHT // 2, WIDTH - 30, HEIGHT // 2 + PADDLE_HEIGHT // 2, fill = "white")
 
-        # ball
         self.ball = self.canvas.create_oval(WIDTH // 2 - BALL_SIZE, HEIGHT // 2 - BALL_SIZE, WIDTH // 2 + BALL_SIZE, HEIGHT // 2 + BALL_SIZE, fill = "white")
 
-        # velocity
+        self.paddle_speed = 4
+        self.ai_speed = 6
+
         self.dx = 4
         self.dy = 3
 
-        self.update()
+        self.w_pressed = False
+        self.s_pressed = False
 
+        self.root.bind("<KeyPress-w>", self.key_down)
+        self.root.bind("<KeyRelease-w>", self.key_up)
+        self.root.bind("<KeyPress-s>", self.key_down)
+        self.root.bind("<KeyRelease-s>", self.key_up)
+
+        self.game_loop()
+
+        self.update()
         self.root.mainloop()
+    
+    def key_down(self, event):
+        if event.keysym == "w":
+            self.w_pressed = True
+        elif event.keysym == "s":
+            self.s_pressed = True
+
+    def key_up(self, event):
+        if event.keysym == "w":
+            self.w_pressed = False
+        elif event.keysym == "s":
+            self.s_pressed = False
+
+    def move_player(self):
+        x1, y1, x2, y2 = self.canvas.coords(self.player)
+
+        if self.w_pressed and y1 > 0:
+            self.canvas.move(self.player, 0, -self.paddle_speed)
+
+        if self.s_pressed and y2 < HEIGHT:
+            self.canvas.move(self.player, 0, self.paddle_speed)
+
+    def move_enemy(self):
+        ex1, ey1, ex2, ey2 = self.canvas.coords(self.enemy)
+
+        bx1, by1, bx2, by2 = self.canvas.coords(self.ball)
+        ball_center = (by1 + by2) / 2
+        enemy_center = (ey1 + ey2) / 2
+
+        diff = ball_center - enemy_center
+
+        if abs(diff) > 5:
+            move = max(-self.ai_speed, min(self.ai_speed, diff))
+            self.canvas.move(self.enemy, 0, move)
+
+        ex1, ey1, ex2, ey2 = self.canvas.coords(self.enemy)
+
+        if ey1 < 0:
+            self.canvas.move(self.enemy, 0, -ey1)
+        if ey2 > HEIGHT:
+            self.canvas.move(self.enemy, 0, HEIGHT - ey2)
+
+    def check_collision(self, paddle):
+        bx1, by1, bx2, by2 = self.canvas.coords(self.ball)
+        px1, py1, px2, py2 = self.canvas.coords(paddle)
+
+        return (
+            bx2 >= px1 and
+            bx1 <= px2 and
+            by2 >= py1 and
+            by1 <= py2
+        )
 
     def update(self):
-        # move ball
+        self.move_player()
+        self.move_enemy()
+
         self.canvas.move(self.ball, self.dx, self.dy)
 
-        x1, y1, x2, y2 = self.canvas.coords(self.ball)
+        bx1, by1, bx2, by2 = self.canvas.coords(self.ball)
 
-        # bounce top/bottom
-        if y1 <= 0 or y2 >= HEIGHT:
+        if by1 <= 0 or by2 >= HEIGHT:
             self.dy = -self.dy
 
-        # bounce left/right (reset for now)
-        if x1 <= 0 or x2 >= WIDTH:
+        if self.check_collision(self.player) and self.dx < 0:
+            self.dx = -self.dx
+
+        if self.check_collision(self.enemy) and self.dx > 0:
+            self.dx = -self.dx
+
+        if bx1 <= 0 or bx2 >= WIDTH:
             self.canvas.coords(self.ball, WIDTH // 2 - BALL_SIZE, HEIGHT // 2 - BALL_SIZE, WIDTH // 2 + BALL_SIZE, HEIGHT // 2 + BALL_SIZE)
             self.dx = -self.dx
 
-        # loop
         self.root.after(16, self.update)
 
+    def game_loop(self):
+        self.move_player()
+        self.root.after(16, self.game_loop)
 
 Game()
